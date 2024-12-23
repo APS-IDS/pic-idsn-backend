f61721a834092f6f616eed50ace5a2209e4c9e74
Jhonatan Caicedo <jonatan9911@gmail.com>
2024-12-23 13:30:35 -0500

    feat(): add labels endpoint

--- src/api/anexo-tecnico/controllers/anexo-tecnico.ts
+++ src/api/anexo-tecnico/controllers/anexo-tecnico.ts
@@ -2,6 +2,14 @@
  * anexo-tecnico controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
+import { FORM_LABELS } from "../../../common/static/form-labels.static";
 
-export default factories.createCoreController('api::anexo-tecnico.anexo-tecnico');
+export default factories.createCoreController(
+  "api::anexo-tecnico.anexo-tecnico",
+  {
+    labels: async (ctx) => {
+      return FORM_LABELS;
+    },
+  }
+);
