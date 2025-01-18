ae3150727440aa39998d9b31995dafa4ef76f554
Jhonatan Caicedo <jonatan9911@gmail.com>
2025-01-18 16:23:58 -0500

    feat(): add upload files endpoint

--- src/api/seguimiento/controllers/seguimiento.ts
+++ src/api/seguimiento/controllers/seguimiento.ts
@@ -6,21 +6,23 @@ export default factories.createCoreController(
   ({ strapi }: { strapi: Core.Strapi }) => ({
     async uploadFile(ctx) {
       const user = ctx.state.user;
+      const files = ctx.request["files"].files;
       console.log(ctx.request.body);
 
       const { anexo_id, soporte_id, municipio_id } = ctx.request.body;
 
-      const response = await strapi
-        .documents("api::seguimiento.seguimiento")
-        .create({
-          data: {
-            municipio: {
-              documentId: municipio_id,
-            },
-          },
+      const result = await strapi
+        .service("api::seguimiento.seguimiento")
+        .uploadFile({
+          anexo_id,
+          soporte_id,
+          municipio_id,
+          user,
+          ctx,
+          files,
         });
 
-      return response;
+      return result;
     },
   })
 );
--- src/api/seguimiento/services/seguimiento.ts
+++ src/api/seguimiento/services/seguimiento.ts
@@ -1,7 +1,107 @@
-/**
- * seguimiento service
- */
+import { factories } from "@strapi/strapi";
+import { Context } from "koa";
 
-import { factories } from '@strapi/strapi';
+export default factories.createCoreService(
+  "api::seguimiento.seguimiento",
+  ({ strapi }) => ({
+    async uploadFile({
+      anexo_id,
+      soporte_id,
+      municipio_id,
+      user,
+      ctx,
+      files,
+    }: {
+      anexo_id: string;
+      soporte_id: string;
+      municipio_id: string;
+      user: any;
+      files: any;
+      ctx: Context;
+    }) {
+      const anexo = await strapi
+        .documents("api::anexo-tecnico.anexo-tecnico")
+        .findFirst({
+          filters: {
+            $and: [
+              {
+                documentId: anexo_id,
+              },
+              {
+                eventos: {
+                  productos: {
+                    actividades: {
+                      soportes: {
+                        id: soporte_id,
+                      },
+                    },
+                  },
+                },
+              },
+            ],
+          },
+          populate: {
+            eventos: {
+              populate: {
+                productos: {
+                  populate: {
+                    actividades: {
+                      populate: {
+                        soportes: true,
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        });
 
-export default factories.createCoreService('api::seguimiento.seguimiento');
+      const municipio = await strapi
+        .documents("api::municipio.municipio")
+        .findOne({
+          documentId: municipio_id,
+        });
+
+      if (!anexo) {
+        return ctx.notFound("Anexo or soporte no encontrado");
+      }
+
+      if (!files) {
+        return ctx.badRequest("No hay archivos para subir");
+      }
+
+      if (!municipio) {
+        return ctx.badRequest("Municipio no encontrado");
+      }
+
+      const data = {
+        fileInfo: {
+          name: files["name"],
+          folder: null,
+        },
+      };
+
+      const uploadedFile = await strapi
+        .service("plugin::upload.upload")
+        .upload({
+          data,
+          files: files,
+        });
+
+      const response = await strapi
+        .documents("api::seguimiento.seguimiento")
+        .create({
+          data: {
+            municipio: {
+              documentId: municipio_id,
+            },
+            soporte_id: soporte_id,
+            archivos: uploadedFile,
+          },
+        });
+
+      return response;
+    },
+  })
+);
