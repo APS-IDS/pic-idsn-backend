6f3157d621fe8f960242a71b4c574cb37e1aa42e
Jhonatan Caicedo <jonatan9911@gmail.com>
2025-02-11 17:35:07 -0500

    feat(): add por definit option for operator

---
+++ database/seeders/04-operadoresPIC_V2.ts
@@ -0,0 +1,10 @@
+export const tableName = "api::operador-pic.operador-pic";
+
+export const data = [
+  {
+    nit: "0",
+    operador_pic: "Por definir",
+    departamento: "SIN ESPECIFICAR",
+    municipio: "SIN ESPECIFICAR",
+  },
+];
