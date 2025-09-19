104e12f6c4e010cde18c94a412308edc4d32233c
Jhonatan Caicedo <jonatan9911@gmail.com>
2025-09-19 17:48:33 -0500

    feat(): add logger

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@
 
 import { Core } from "@strapi/strapi";
 import runSeeders from "../database/helpers/runSeeders";
+import logger from "koa-pino-logger";
+import { Logger } from "pino";
+import fs from "fs";
+import path from "path";
 
 export default {
   /**
@@ -19,7 +23,31 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  async bootstrap({ strapi }: { strapi: Core.Strapi }) {
+  async bootstrap({
+    strapi,
+  }: {
+    strapi: Core.Strapi & { pinoLogger: Logger };
+  }) {
+    try {
+      const logDir = path.join(__dirname, "../..", "logs");
+      if (!fs.existsSync(logDir)) {
+        fs.mkdirSync(logDir);
+      }
+      console.log("Log directory:", logDir);
+      const logFile = fs.createWriteStream(path.join(logDir, "app.log"), {
+        flags: "a",
+      });
+
+      const pinoLogger = logger({
+        level: "trace",
+        stream: logFile,
+      });
+
+      strapi.pinoLogger = pinoLogger.logger;
+      strapi.server.app.use(pinoLogger);
+    } catch (err) {
+      console.error("Failed to set up logger:", err);
+    }
     await runSeeders(strapi);
   },
 };
