f150e5451c7a375119ae3c64b86cd571b7dcb16d
Jhonatan Caicedo <jonatan9911@gmail.com>
2026-02-13 16:01:42 -0500

    feat() add size file limit

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,11 +1,20 @@
-export default [
+export default ({ env }) => [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
-  'strapi::body',
+  {
+    name: 'strapi::body',
+    config: {
+      formidable: {
+        // bytes; default 10 MB if env not set
+        maxFileSize: env.int('MAX_FILE_SIZE', 10 * 1024 * 1024),
+        maxFiles: 1
+      },
+    },
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
