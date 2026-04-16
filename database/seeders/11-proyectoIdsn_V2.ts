b8ff5d2e1f5cc68ecff7d4ccec86a87b4b74cd5f
Jhonatan Caicedo <jonatan9911@gmail.com>
2026-04-16 15:03:30 -0500

    feat() add proyectos seeder

---
+++ database/seeders/11-proyectoIdsn_V2.ts
@@ -0,0 +1,53 @@
+export const tableName = "api::proyectos-idsn.proyectos-idsn";
+
+export const data = [
+  {
+    proyecto: "Enfermedades No Transmisibles - Cronicos",
+    descripcion:
+      "Fortalecimiento de capacidades a los entes territoriales municipales encaminados a promover los hábitos y estilos de vida saludable para la prevención de enfermedades crónicas en el departamento de Nariño",
+  },
+  {
+    proyecto: "Enfermeddes Emergentes - Transmisibles (TB, HANSEN, IRA)",
+    descripcion:
+      "Fortalecimiento de la  Vigilancia Epidemiológica y Gestión del Conocimiento en el Departamento de Nariño",
+  },
+  {
+    proyecto: "Zoonosis - Salud Ambiental",
+    descripcion: "Atencion a siuaciones medioambientales, entornnos saludables",
+  },
+  {
+    proyecto: " Discapacidad - Poblacion de especial proteccion",
+    descripcion:
+      "Fortalecimiento de estrategias para la implementación de normas, guías y protocolos basados en enfoque  diferencial que  garanticen el derecho a la salud de la población de especial protección Nariño",
+  },
+  {
+    proyecto: "Salud Infantil - Poblacion de especial proteccion",
+    descripcion:
+      "Fortalecimiento de estrategias para la implementación de normas, guías y protocolos basados en enfoque  diferencial que  garanticen el derecho a la salud de la población de especial protección Nariño",
+  },
+  {
+    proyecto: "Envejecimiento y vejez -  Poblacion de especial proteccion",
+    descripcion:
+      "Fortalecimiento de estrategias para la implementación de normas, guías y protocolos basados en enfoque  diferencial que  garanticen el derecho a la salud de la población de especial protección Nariño",
+  },
+  {
+    proyecto: "Victimas - Poblacion de especial proteccion",
+    descripcion:
+      "Fortalecimiento de estrategias para la implementación de normas, guías y protocolos basados en enfoque  diferencial que  garanticen el derecho a la salud de la población de especial protección Nariño",
+  },
+  {
+    proyecto: "Etnias - Poblacion de especial proteccio",
+    descripcion:
+      "Fortalecimiento de estrategias para la implementación de normas, guías y protocolos basados en enfoque  diferencial que  garanticen el derecho a la salud de la población de especial protección Nariño",
+  },
+  {
+    proyecto: "Vigilancia Epidemiologica",
+    descripcion:
+      "Fortalecimiento de la  Vigilancia Epidemiológica y Gestión del Conocimiento en el Departamento de Nariño",
+  },
+  {
+    proyecto: "Laboratorio de Salud Publica - Hemoderivados",
+    descripcion: "Sin dato",
+  },
+  { proyecto: "Comunicaciones", descripcion: "Sin dato" },
+];
